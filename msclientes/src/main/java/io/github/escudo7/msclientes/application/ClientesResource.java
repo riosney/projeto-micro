07bfbf781600822ca5e6545b4bc5708b1e55e7ef
Riosney <rsantosdo@minsait.com>
2023-03-08 09:58:22 -0300

    Criando e testando microservice clientes

--- msclientes/src/main/java/io/github/escudo7/msclientes/application/ClientesResource.java
+++ msclientes/src/main/java/io/github/escudo7/msclientes/application/ClientesResource.java
@@ -1,15 +1,49 @@
 package io.github.escudo7.msclientes.application;
 
+import io.github.escudo7.msclientes.application.representation.ClienteSaveRequest;
+import io.github.escudo7.msclientes.domain.Cliente;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("clientes")
+@RequiredArgsConstructor
 public class ClientesResource {
+
+    private final ClienteService service;
 
     @GetMapping
     public String status(){
         return "Ok";
     }
+
+    @PostMapping
+    public ResponseEntity save(@RequestBody ClienteSaveRequest request){
+        var cliente = request.toModel();
+        service.save(cliente);
+        URI headerLocation = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .query("cpf={cpf}")
+                .buildAndExpand(cliente.getCpf())
+                .toUri();
+        return ResponseEntity.created(headerLocation).build();
+    }
+    @GetMapping(params = "cpf")
+    public ResponseEntity dadosCliente(@RequestParam("cpf") String cpf){
+        var cliente = service.getByCPF(cpf);
+        if(cliente.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(cliente);
+    }
+
 }
